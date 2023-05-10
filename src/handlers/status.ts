fa0f81730f85bbe6e1b7fe52e63c10c90a90206b
eabz <eabz@kindynos.mx>
2023-05-09 19:59:11 -0600

    Reduce data

--- src/handlers/status.ts
+++ src/handlers/status.ts
@@ -14,12 +14,7 @@ export class Status extends OpenAPIRoute {
               blocks: new Str(),
               chain: new Str(),
               contracts: new Str(),
-              dex_trades: new Str(),
-              erc20_transfers: new Str(),
-              erc721_transfers: new Str(),
-              erc1155_transfers: new Str(),
               logs: new Str(),
-              receipts: new Str(),
               traces: new Str(),
               transactions: new Str(),
               withdrawals: new Str(),
@@ -44,12 +39,7 @@ export class Status extends OpenAPIRoute {
       { data: blocksIndexed },
       { data: transactionsIndexed },
       { data: contractsIndexed },
-      { data: receiptsIndexed },
       { data: logsIndexed },
-      { data: dexTrades },
-      { data: erc20Transfers },
-      { data: erc721Transfers },
-      { data: erc1155Transfers },
       { data: tracesIndexed },
       { data: withdrawalsIndexed },
     ] = await Promise.all([
@@ -65,27 +55,8 @@ export class Status extends OpenAPIRoute {
         env,
         'SELECT count(*) as contracts, chain FROM indexer.contracts GROUP BY chain',
       ),
-      query<{ chain: number; receipts: number }>(
-        env,
-        'SELECT count(*) as receipts, chain FROM indexer.receipts GROUP BY chain',
-      ),
       query<{ chain: number; logs: number }>(env, 'SELECT count(*) as logs, chain FROM indexer.logs GROUP BY chain'),
-      query<{ chain: number; dex_trades: number }>(
-        env,
-        'SELECT count(*) as dex_trades, chain FROM indexer.dex_trades GROUP BY chain',
-      ),
-      query<{ chain: number; erc20_transfers: number }>(
-        env,
-        'SELECT count(*) as erc20_transfers, chain FROM indexer.erc20_transfers GROUP BY chain',
-      ),
-      query<{ chain: number; erc721_transfers: number }>(
-        env,
-        'SELECT count(*) as erc721_transfers, chain FROM indexer.erc721_transfers GROUP BY chain',
-      ),
-      query<{ chain: number; erc1155_transfers: number }>(
-        env,
-        'SELECT count(*) as erc1155_transfers, chain FROM indexer.erc1155_transfers GROUP BY chain',
-      ),
+
       query<{ chain: number; traces: number }>(
         env,
         'SELECT count(*) as traces, chain FROM indexer.traces GROUP BY chain',
@@ -101,19 +72,7 @@ export class Status extends OpenAPIRoute {
 
       const chainContracts = contractsIndexed?.find((items) => items.chain === chainInfo.chain)?.contracts || 0
 
-      const chainReceipts = receiptsIndexed?.find((items) => items.chain === chainInfo.chain)?.receipts || 0
-
       const chainLogs = logsIndexed?.find((items) => items.chain === chainInfo.chain)?.logs || 0
-
-      const chainDexTrades = dexTrades?.find((items) => items.chain === chainInfo.chain)?.dex_trades || 0
-
-      const chainErc20Transfers = erc20Transfers?.find((items) => items.chain === chainInfo.chain)?.erc20_transfers || 0
-
-      const chainErc721Transfers =
-        erc721Transfers?.find((items) => items.chain === chainInfo.chain)?.erc721_transfers || 0
-
-      const chainErc1155Transfers =
-        erc1155Transfers?.find((items) => items.chain === chainInfo.chain)?.erc1155_transfers || 0
 
       const chainTraces = tracesIndexed?.find((items) => items.chain === chainInfo.chain)?.traces || 0
 
@@ -122,12 +81,7 @@ export class Status extends OpenAPIRoute {
       return {
         ...chainInfo,
         contracts: chainContracts,
-        dex_trades: chainDexTrades,
-        erc20_transfers: chainErc20Transfers,
-        erc721_transfers: chainErc721Transfers,
-        erc1155_transfers: chainErc1155Transfers,
         logs: chainLogs,
-        receipts: chainReceipts,
         traces: chainTraces,
         transactions: chainTransactions,
         withdrawals: chainWithdrawals,
